93621788a841ba04cef4f4c8cfc3d17d4cd37436
danielriveros07 <danielriveros07@gmail.com>
2025-06-25 21:06:18 -0300

    Update schema.sql

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -12,8 +12,9 @@ CREATE TABLE usuario (
 );
 
 -- Insertar los datos
-INSERT INTO usuario (id,nombre, email, clave, rol, fecha_baja) VALUES
-(1, 'Marcelo Murgo','cmmurgo@gmail.com', '$2b$10$Dg/pOEjkYFP2F5vtFJugped7tRoaUoVkHAiG/JM1wD.N/gaQHNHOC', 'admin', null);
+
+INSERT INTO usuario (nombre, email, clave, rol, fecha_baja) VALUES
+('Marcelo Murgo','cmmurgo@gmail.com', '$2b$10$Dg/pOEjkYFP2F5vtFJugped7tRoaUoVkHAiG/JM1wD.N/gaQHNHOC', 'admin', null);
 
 CREATE TABLE cliente (
     id SERIAL PRIMARY KEY,
